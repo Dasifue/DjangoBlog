59d8ec4b5b911b279669015c4376ac327d8b6872
dasifue <dasifue@gmail.com>
2024-12-13 00:43:21 +0600

    Added: products page

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 
 from .models import Publication, Category, Tag
 
 
 def home(request):
+
+    return render(request=request, template_name='index.html')
+
+
+def get_publications(request):
+    publications = Publication.objects.all().order_by('-created')
+    paginator = Paginator(object_list=publications, per_page=12)
+
+    page = request.GET.get('page', 1)
+    publications = paginator.get_page(number=page)    
+
     context = {
-        'categories': Category.objects.all(),
-        'tags': Tag.objects.all(),
-        'publications': Publication.objects.all().order_by('-created')[:5]
+        'publications': publications
     }
-    return render(request=request, template_name='index.html', context=context)
+    return render(request=request, template_name='publications.html', context=context)
